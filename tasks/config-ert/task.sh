711000b1fa1e1374e443b54c50f98113c660e855
Tho Duy Nguyen <tho.duy.nguyen@desjardins.com>
2018-04-23 16:12:32 -0400

    container networking type to external instead of Silk

--- tasks/config-ert/task.sh
+++ tasks/config-ert/task.sh
@@ -122,8 +122,8 @@ cf_properties=$(
       ".properties.logger_endpoint_port": {
         "value": $loggregator_endpoint_port
       },
-      ".properties.container_networking_interface_plugin.silk.network_cidr": {
-        "value": $container_networking_nw_cidr
+      ".properties.container_networking_interface_plugin": {
+        "value": "external"
       },
       ".properties.security_acknowledgement": {
         "value": $security_acknowledgement
